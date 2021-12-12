7706b404102954b1465827f8af97282559d9b894
Jonathan Giles <jonathan@jonathangiles.net>
2021-12-13 12:31:24 +1300

    Updating module-info to be correct, and updating dependencies in pom.xml to enable the new release to be made.

--- src/main/java/net/jonathangiles/tools/sitebuilder/module-info.java
+++ src/main/java/net/jonathangiles/tools/sitebuilder/module-info.java
@@ -1,5 +1,12 @@
 module net.jonathangiles.tools.sitebuilder {
-    requires net.jonathangiles.tools.teenyhttpd;
+    requires org.commonmark;
+    requires org.commonmark.ext.front.matter;
+
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
+    requires com.fasterxml.jackson.dataformat.xml;
 
     exports net.jonathangiles.tools.sitebuilder;
     exports net.jonathangiles.tools.sitebuilder.models;
